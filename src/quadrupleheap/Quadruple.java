aa8b9e48af7514548a0e9f74bd94a4932a352771
Suyash Patel <suyash@Suyashs-MacBook-Air.local>
2022-03-08 15:57:08 -0700

    Completed the quadruple heap file flow - added code for TScan

--- src/quadrupleheap/Quadruple.java
+++ src/quadrupleheap/Quadruple.java
@@ -2,6 +2,9 @@ package quadrupleheap;
 
 import global.*;
 import heap.FieldNumberOutOfBoundException;
+import heap.InvalidTupleSizeException;
+import heap.InvalidTypeException;
+import heap.Tuple;
 
 import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
@@ -17,7 +20,7 @@ public class Quadruple implements GlobalConst {
     private byte [] data;
     private int quadruple_offset;
     private int quadruple_length;
-    private final short fldCnt = 4;
+    private short fldCnt = 4;
     private short [] fldOffset;
 
     public Quadruple() {
@@ -204,5 +207,185 @@ public class Quadruple implements GlobalConst {
 
     public void setValue(double value) {
         this.value = value;
+    }
+
+
+    public int getIntFld(int fldNo)
+            throws IOException, FieldNumberOutOfBoundException {
+        int val;
+        if ( (fldNo > 0) && (fldNo <= fldCnt))
+        {
+            val = Convert.getIntValue(fldOffset[fldNo -1], data);
+            return val;
+        }
+        else
+            throw new FieldNumberOutOfBoundException (null, "TUPLE:TUPLE_FLDNO_OUT_OF_BOUND");
+    }
+
+    public float getFloFld(int fldNo)
+            throws IOException, FieldNumberOutOfBoundException {
+        float val;
+        if ( (fldNo > 0) && (fldNo <= fldCnt))
+        {
+            val = Convert.getFloValue(fldOffset[fldNo -1], data);
+            return val;
+        }
+        else
+            throw new FieldNumberOutOfBoundException (null, "TUPLE:TUPLE_FLDNO_OUT_OF_BOUND");
+    }
+
+    public String getStrFld(int fldNo)
+            throws IOException, FieldNumberOutOfBoundException {
+        String val;
+        if ( (fldNo > 0) && (fldNo <= fldCnt))
+        {
+            val = Convert.getStrValue(fldOffset[fldNo -1], data,
+                    fldOffset[fldNo] - fldOffset[fldNo -1]); //strlen+2
+            return val;
+        }
+        else
+            throw new FieldNumberOutOfBoundException (null, "TUPLE:TUPLE_FLDNO_OUT_OF_BOUND");
+    }
+
+    public char getCharFld(int fldNo)
+            throws IOException, FieldNumberOutOfBoundException {
+        char val;
+        if ( (fldNo > 0) && (fldNo <= fldCnt))
+        {
+            val = Convert.getCharValue(fldOffset[fldNo -1], data);
+            return val;
+        }
+        else
+            throw new FieldNumberOutOfBoundException (null, "TUPLE:TUPLE_FLDNO_OUT_OF_BOUND");
+
+    }
+
+    public Quadruple setIntFld(int fldNo, int val)
+            throws IOException, FieldNumberOutOfBoundException {
+        if ( (fldNo > 0) && (fldNo <= fldCnt))
+        {
+            Convert.setIntValue (val, fldOffset[fldNo -1], data);
+            return this;
+        }
+        else
+            throw new FieldNumberOutOfBoundException (null, "TUPLE:TUPLE_FLDNO_OUT_OF_BOUND");
+    }
+
+    public Quadruple setFloFld(int fldNo, float val)
+            throws IOException, FieldNumberOutOfBoundException {
+        if ( (fldNo > 0) && (fldNo <= fldCnt))
+        {
+            Convert.setFloValue (val, fldOffset[fldNo -1], data);
+            return this;
+        }
+        else
+            throw new FieldNumberOutOfBoundException (null, "TUPLE:TUPLE_FLDNO_OUT_OF_BOUND");
+
+    }
+
+    public Quadruple setStrFld(int fldNo, String val)
+            throws IOException, FieldNumberOutOfBoundException {
+        if ( (fldNo > 0) && (fldNo <= fldCnt)) {
+            Convert.setStrValue (val, fldOffset[fldNo -1], data);
+            return this;
+        }
+        else
+            throw new FieldNumberOutOfBoundException (null, "TUPLE:TUPLE_FLDNO_OUT_OF_BOUND");
+    }
+
+
+    /**
+     * setHdr will set the header of this tuple.
+     *
+     * @param	numFlds	  number of fields
+     * @param	types[]	  contains the types that will be in this tuple
+     * @param	strSizes[]      contains the sizes of the string
+     *
+     * @exception IOException I/O errors
+     * @exception InvalidTypeException Invalid tupe type
+     * @exception InvalidTupleSizeException Tuple size too big
+     *
+     */
+
+    public void setHdr (short numFlds,  AttrType types[], short strSizes[])
+            throws IOException, InvalidTypeException, InvalidTupleSizeException
+    {
+        if((numFlds +2)*2 > max_size)
+            throw new InvalidTupleSizeException (null, "TUPLE: TUPLE_TOOBIG_ERROR");
+
+        fldCnt = numFlds;
+        Convert.setShortValue(numFlds, quadruple_offset, data);
+        fldOffset = new short[numFlds+1];
+        int pos = quadruple_offset+2;  // start position for fldOffset[]
+
+        //sizeof short =2  +2: array siaze = numFlds +1 (0 - numFilds) and
+        //another 1 for fldCnt
+        fldOffset[0] = (short) ((numFlds +2) * 2 + quadruple_offset);
+
+        Convert.setShortValue(fldOffset[0], pos, data);
+        pos +=2;
+        short strCount =0;
+        short incr;
+        int i;
+
+        for (i=1; i<numFlds; i++)
+        {
+            switch(types[i-1].attrType) {
+
+                case AttrType.attrInteger:
+                    incr = 4;
+                    break;
+
+                case AttrType.attrReal:
+                    incr =4;
+                    break;
+
+                case AttrType.attrString:
+                    incr = (short) (strSizes[strCount] +2);  //strlen in bytes = strlen +2
+                    strCount++;
+                    break;
+
+                default:
+                    throw new InvalidTypeException (null, "TUPLE: TUPLE_TYPE_ERROR");
+            }
+            fldOffset[i]  = (short) (fldOffset[i-1] + incr);
+            Convert.setShortValue(fldOffset[i], pos, data);
+            pos +=2;
+
+        }
+        switch(types[numFlds -1].attrType) {
+
+            case AttrType.attrInteger:
+                incr = 4;
+                break;
+
+            case AttrType.attrReal:
+                incr =4;
+                break;
+
+            case AttrType.attrString:
+                incr =(short) ( strSizes[strCount] +2);  //strlen in bytes = strlen +2
+                break;
+
+            default:
+                throw new InvalidTypeException (null, "TUPLE: TUPLE_TYPE_ERROR");
+        }
+
+        fldOffset[numFlds] = (short) (fldOffset[i-1] + incr);
+        Convert.setShortValue(fldOffset[numFlds], pos, data);
+
+        quadruple_length = fldOffset[numFlds] - quadruple_offset;
+
+        if(quadruple_length > max_size)
+            throw new InvalidTupleSizeException (null, "TUPLE: TUPLE_TOOBIG_ERROR");
+    }
+
+    public byte [] returnQuadrupleByteArray()
+    {
+        return data;
+    }
+
+    public int getOffset() {
+        return quadruple_offset;
     }
 }
