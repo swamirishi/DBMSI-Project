790f91dda0a89bd1912e580660c763000142aa17
Abhishek Jindal <abhishekjindal21@gmail.com>
2022-03-13 03:39:12 -0700

    Changes for RDFDB and Stream

--- src/diskmgr/RDFDB.java
+++ src/diskmgr/RDFDB.java
@@ -1,62 +1,141 @@
 package diskmgr;
 
-import global.EID;
-import global.PID;
+import btree.BTIndexPage;
+import global.*;
+import heap.*;
+import labelheap.LScan;
+import labelheap.Label;
+import labelheap.LabelHeapFile;
+import quadrupleheap.Quadruple;
+import quadrupleheap.QuadrupleHeapFile;
+import quadrupleheap.TScan;
+
+import java.io.IOException;
+import java.util.Arrays;
 
 public class RDFDB extends DB {
+    private QuadrupleHeapFile quadrupleHeapFile;
+    private LabelHeapFile entityLabelHeapFile;
+    private LabelHeapFile predicateLabelHeapFile;
+    private int subjectCount = 0;
+    private int objectCount = 0;
+
     public RDFDB(int type) {
-
+//        BTIndexPage btIndexPage = new BTIndexPage();
+        try {
+            quadrupleHeapFile = new QuadrupleHeapFile("quadrupleHeapFile");
+            entityLabelHeapFile = new LabelHeapFile("entityLabelHeapFile");
+            predicateLabelHeapFile = new LabelHeapFile("predicateLabelHeapFile");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    public int getQuadrupleCnt() {
-        return 0;
+    public int getQuadrupleCnt() throws HFDiskMgrException, InvalidSlotNumberException, InvalidTupleSizeException, HFBufMgrException, IOException {
+        return quadrupleHeapFile.getRecCnt();
     }
 
-    public int getEntityCnt() {
-        return 0;
+    public int getEntityCnt() throws HFDiskMgrException, InvalidSlotNumberException, InvalidTupleSizeException, HFBufMgrException, IOException {
+        return entityLabelHeapFile.getRecCnt();
     }
 
-    public int getPredicateCnt() {
-        return 0;
+    public int getPredicateCnt() throws HFDiskMgrException, InvalidSlotNumberException, InvalidTupleSizeException, HFBufMgrException, IOException {
+        return predicateLabelHeapFile.getRecCnt();
     }
 
     public int getSubjectCnt() {
-        return 0;
+        return subjectCount;
     }
 
     public int getObjectCnt() {
-        return 0;
+        return objectCount;
     }
 
     //Need to return EID
     public EID insertEntity(String entityLabel) {
-        return null;
+        try {
+            LID lid = getLIDFromHeapFileScan(entityLabel);
+            if (lid.getPageNo().pid == INVALID_PAGE) {
+                lid = entityLabelHeapFile.insertRecord(entityLabel.getBytes());
+            }
+            return lid.returnEid();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     public boolean deleteEntity(String entityLabel) {
-        return false;
+        try {
+            LID lid = getLIDFromHeapFileScan(entityLabel);
+            return entityLabelHeapFile.deleteRecord(lid);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
-    //Need to return PID
-    public PID insertPredicate(String predicateLabel) {
-        return null;
+    public PID insertPredicate(String predicateLabel) throws SpaceNotAvailableException, HFDiskMgrException, HFException, InvalidSlotNumberException, InvalidTupleSizeException, HFBufMgrException, IOException {
+        LID lid = predicateLabelHeapFile.insertRecord(predicateLabel.getBytes());
+        return lid.returnPid();
     }
 
-    public boolean deletePredicate(String predicateLabel) {
-        return false;
+    public boolean deletePredicate(String predicateLabel) throws Exception {
+        LID lid = getLIDFromHeapFileScan(predicateLabel);
+        return predicateLabelHeapFile.deleteRecord(lid);
     }
 
     //Need to return QID. Change type void to QID
-    public void insertQuadruple(byte[] quadruplePtr) {
-        return;
+    public QID insertQuadruple(byte[] quadruplePtr) throws SpaceNotAvailableException, HFDiskMgrException, HFException, InvalidSlotNumberException, InvalidTupleSizeException, HFBufMgrException, IOException {
+        return quadrupleHeapFile.insertRecord(quadruplePtr);
     }
 
-    public boolean deleteQuadruple(byte[] quadruplePtr) {
-        return false;
+    public boolean deleteQuadruple(byte[] quadruplePtr) throws Exception {
+        QID qid = getQIDFromHeapFileScan(quadruplePtr);
+        return quadrupleHeapFile.deleteRecord(qid);
     }
 
     public Stream openStream(int orderType, String subjectFilter, String predicateFilter,
-                             String objectFilter, double confidenceFilter){
+                             String objectFilter, double confidenceFilter) {
         return null;
+    }
+
+    private LID getLIDFromHeapFileScan(String inputLabel) throws InvalidTupleSizeException, IOException {
+        LID lid = new LID();
+        LScan scan = entityLabelHeapFile.openScan();
+        boolean isFound = false;
+        while (true) {
+            Label label = scan.getNext(lid);
+            if (label != null && Arrays.equals(inputLabel.getBytes(), label.getLabelByteArray())) {
+                System.out.println(inputLabel);
+                isFound = true;
+                break;
+            }
+        }
+        if (!isFound) {
+            lid.getPageNo().pid = INVALID_PAGE;
+        }
+        return lid;
+    }
+
+    private QID getQIDFromHeapFileScan(byte[] inputData) throws InvalidTupleSizeException, IOException {
+        QID qid = new QID();
+        TScan scan = quadrupleHeapFile.openScan();
+        boolean isFound = false;
+        while (true) {
+            Quadruple quadruple = scan.getNext(qid);
+            if (quadruple != null && Arrays.equals(inputData, quadruple.getQuadrupleByteArray())) {
+                isFound = true;
+                break;
+            }
+        }
+        if (!isFound) {
+            qid.getPageNo().pid = INVALID_PAGE;
+        }
+        return qid;
+    }
+
+    public QuadrupleHeapFile getQuadrupleHeapFile(){
+        return this.quadrupleHeapFile;
     }
 }
