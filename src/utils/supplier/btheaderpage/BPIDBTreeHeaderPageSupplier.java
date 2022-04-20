bfbbda5ccd5b33178f5bad9c54324a74565264da
Mohd Zaid <diaz.zidane@gmail.com>
2022-04-20 12:36:55 -0700

    Add files via upload

---
+++ src/utils/supplier/btheaderpage/BPIDBTreeHeaderPageSupplier.java
@@ -0,0 +1,40 @@
+package utils.supplier.btheaderpage;
+
+import btree.ConstructPageException;
+import btree.basicpattern.BPIDBTreeHeaderPage;
+import diskmgr.Page;
+import global.PageId;
+import global.BPID;
+import basicpattern.BasicPattern;
+
+public class BPIDBTreeHeaderPageSupplier implements BTreeHeaderPageSupplier<BPID, BasicPattern> {
+    @Override
+    public BPIDBTreeHeaderPage getBTreeHeaderPage(PageId pageId) throws ConstructPageException {
+        return new BPIDBTreeHeaderPage(pageId);
+    }
+
+    @Override
+    public BPIDBTreeHeaderPage getBTreeHeaderPage(Page page){
+        return new BPIDBTreeHeaderPage(page);
+    }
+
+    @Override
+    public BPIDBTreeHeaderPage getBTreeHeaderPage() throws ConstructPageException {
+        return new BPIDBTreeHeaderPage();
+    }
+
+    private BPIDBTreeHeaderPageSupplier() {
+    }
+    
+    private static BPIDBTreeHeaderPageSupplier supplier;
+    public static BPIDBTreeHeaderPageSupplier getSupplier(){
+        if(supplier == null){
+            synchronized (BPIDBTreeHeaderPageSupplier.class){
+                if(supplier == null){
+                    supplier = new BPIDBTreeHeaderPageSupplier();
+                }
+            }
+        }
+        return supplier;
+    }
+}
